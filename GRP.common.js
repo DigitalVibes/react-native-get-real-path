fb5a11fc2f87a0ebec0a93130baba6458ddba1ca
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2017-12-03 21:59:00 +0100

    fix: ios-runtime errors resolved

--- GRP.common.js
+++ GRP.common.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // This file supports both iOS and Android
-
+import {
+  Platform
+} from 'react-native';
 // Stop bluebird going nuts because it can't find "self"
 if (typeof self === 'undefined') {
   global.self = global;
@@ -11,18 +13,19 @@ var GRP = require('react-native').NativeModules.GRP;
 var NativeAppEventEmitter = require('react-native').NativeAppEventEmitter;
 var Promise = require('bluebird');
 
-var _getRealPathFromURI = Promise.promisify(GRP ? GRP.getRealPathFromURI : (fileUri) => { return fileUri; });
+if (Platform.OS === 'android') {
+  var _getRealPathFromURI = Promise.promisify(GRP.getRealPathFromURI);
 
-var convertError = (err) => {
-  if (err.isOperational && err.cause) {
-    err = err.cause;
-  }
+  var convertError = (err) => {
+    if (err.isOperational && err.cause) {
+      err = err.cause;
+    }
 
-  var error = new Error(err.description || err.message);
-  error.code = err.code;
-  throw error;
-};
-
+    var error = new Error(err.description || err.message);
+    error.code = err.code;
+    throw error;
+  };
+}
 var RNGRP = {
   getRealPathFromURI(fileUri) {
     return _getRealPathFromURI(fileUri)
